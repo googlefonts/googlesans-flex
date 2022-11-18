d0f9e114fa3a3e182a0aa75b4bf084252152ac5c
Ricky Atkins <ricky.atkins@daltonmaag.com>
2022-11-18 16:36:27 +0000

    Generalise branch to compare to

--- .github/actions/ufodiff/entrypoint.sh
+++ .github/actions/ufodiff/entrypoint.sh
@@ -10,8 +10,10 @@ git config --global --add safe.directory /github/workspace
 # Change to repo location as otherwise ufodiff dies
 cd /github/workspace
 
-git fetch origin main &> /dev/null
-git branch main origin/main &> /dev/null
+# Re-parse branch from 3rd arg, which will be branch:name (see action.yml)
+branch=$(echo $3 | cut -d : -f 2)
+git fetch origin "$branch" &> /dev/null
+git branch "$branch" "origin/$branch" &> /dev/null
 
 # Call ufodiff with the supplied args
 ufodiff $*
