df0580f2edd6a1def7ed72f77a26abe1cd11eeaf
sannorozco <santiagorozco@gmail.com>
2022-12-01 10:47:24 -0600

    Update roman and italic sources from FB repo
    —roman /6 /9 ROND100
    —italic DB’s ascii progress

--- sources/roman/build-GoogleSansFlex.sh
+++ sources/roman/build-GoogleSansFlex.sh
@@ -1,9 +1,9 @@
 
-source tools/gsflex-env/bin/activate
+source tools/googlesansflex-env/bin/activate
 
 ## MAKE VF
 
-fontmake -m sources/roman/GoogleSansFlex.designspace -o variable --output-path fonts/GoogleSansFlex-Beta1.0[ROND,opsz,wdth,wght]-VF.ttf --no-production-names
+fontmake -m sources/roman/GoogleSansFlex.designspace -o variable --output-path fonts/GoogleSansFlex-Alpha1.0[ROND,opsz,wdth,wght]-VF.ttf --no-production-names
 
 #fontmake -m sources/GoogleSansFlex-Squiggle/GoogleSansFlex-Squiggle.designspace -o variable --output-path fonts/GoogleSansFlex-Squiggle[WHGT,wdth]-VF.ttf --no-production-names
 
