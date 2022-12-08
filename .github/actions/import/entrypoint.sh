026f0dbe4fa58ec7ff2cc7a474287cab6678c4c8
Jany Belluz <jany.belluz@daltonmaag.com>
2022-12-08 18:28:24 +0000

    Change emails to jany.belluz@daltonmaag.com (has signed the CLA)

--- .github/actions/import/entrypoint.sh
+++ .github/actions/import/entrypoint.sh
@@ -24,15 +24,15 @@ echo "Normalising upright designspaces..."
 python3 /scripts/gs-normalize-designspace.py \
     --source-dir /github/workspace/main/sources/regular/
 
-echo "Merging italic designspace..."
-python3 /scripts/gs-merge-designspace.py \
-    --source /github/workspace/staging/sources/italic/GoogleSansFlex-Italic.designspace \
-    --target /github/workspace/main/sources/italic/GoogleSansFlex-Italic.designspace \
-    --import-glyphs-file /glyph-list.txt \
-    --replace-target-designspace \
-    --follow-glyphs # While sources are in flux
-echo
+# echo "Merging italic designspace..."
+# python3 /scripts/gs-merge-designspace.py \
+#     --source /github/workspace/staging/sources/italic/GoogleSansFlex-Italic.designspace \
+#     --target /github/workspace/main/sources/italic/GoogleSansFlex-Italic.designspace \
+#     --import-glyphs-file /glyph-list.txt \
+#     --replace-target-designspace \
+#     --follow-glyphs # While sources are in flux
+# echo
 
-echo "Normalising italic designspaces..."
-python3 /scripts/gs-normalize-designspace.py \
-    --source-dir /github/workspace/main/sources/italic/
+# echo "Normalising italic designspaces..."
+# python3 /scripts/gs-normalize-designspace.py \
+#     --source-dir /github/workspace/main/sources/italic/
