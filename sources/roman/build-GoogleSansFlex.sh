5d039f9b2043a6a615d83af64d5e20ff1d97b76a
Chris Simpkins <chris@sourcefoundry.org>
2022-12-11 21:03:21 -0500

    update Font Bureau roman and italic sources to https://github.com/FontBureau/fb-GoogleSansFlex/commit/ffc2abc4553c9ee06f7671a41a5d52bd7ebdb2c5

---
+++ sources/roman/build-GoogleSansFlex.sh
@@ -0,0 +1,12 @@
+
+source tools/googlesansflex-env/bin/activate
+
+## MAKE VF
+
+fontmake -m sources/roman/GoogleSansFlex.designspace -o variable --output-path fonts/GoogleSansFlex-Alpha1.0[GRAD,ROND,opsz,wdth,wght]-VF.ttf --no-production-names
+
+#fontmake -m sources/italic/Italic-opsz18.designspace -o variable --output-path fonts/GoogleSansFlex-Italic-Alpha1.0[ROND,opsz,wdth,wght]-VF.ttf --no-production-names
+
+#fontmake -m sources/GoogleSansFlex-Squiggle/GoogleSansFlex-Squiggle.designspace -o variable --output-path fonts/GoogleSansFlex-Squiggle[WHGT,wdth]-VF.ttf --no-production-names
+
+deactivate
