6565513d446585c17e8a9956a675f309a043f408
Ricky Atkins <ricky.atkins@daltonmaag.com>
2022-12-15 11:09:07 +0000

    Allow configuration of path for file size

--- .github/actions/file-size/entrypoint.sh
+++ .github/actions/file-size/entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-if [ ! -e /github/workspace/fonts ] ; then
-    echo "ERROR: built fonts not found at /github/workspace/fonts"
+# If $1 is specified, use that as the path
+if [ -n "$1" ] ; then
+    REPO_PATH=$1
+else
+    REPO_PATH="/github/workspace"
+fi
+
+if [ ! -d "$REPO_PATH/fonts" ] ; then
+    echo "ERROR: built fonts not found at $REPO_PATH/fonts"
     exit 1
 fi
 
-# shellcheck disable=SC2038
-find /github/workspace/fonts -name '*.ttf' -type f | xargs python3 /report-filesize.py
+find "$REPO_PATH/fonts" -name '*.ttf' -type f -print0 | xargs -0 python3 /report-filesize.py
