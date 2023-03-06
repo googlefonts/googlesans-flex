ed2019dc4be316f4eb5c7bc152a4ea1cedc5707c
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2023-03-06 12:26:29 +0000

    Fix Unicode lookup

--- scripts/regenerate-glyph-metadata.py
+++ scripts/regenerate-glyph-metadata.py
@@ -23,20 +23,28 @@ import glyphsLib.glyphdata
 
 ROOT_DIR = Path(__file__).parent.parent
 
+
 def main(csv_path: Path):
     glyphdata = ufo_glyphdata_manager.__main__.read_csv(csv_path)
+
     for name, data in glyphdata.items():
+        unicodes = [hex(uv)[2:] for uv in data.unicodes]
+        glyphsapp_data = glyphsLib.glyphdata.get_glyph(name, unicodes=unicodes)
+
         name_sanitized = name.replace("-", "")
-        glyphsapp_data = glyphsLib.glyphdata.get_glyph(name, unicodes=data.unicodes)
         if (prod_name := glyphsapp_data.production_name) != name_sanitized:
             data.postscript_name = prod_name
         elif name != name_sanitized:
-            print(f"INFO: automatically to specified postscript name for {name} by removing dashes")
+            print(
+                f"INFO: automatically to specified postscript name for {name} by "
+                "removing dashes"
+            )
             data.postscript_name = name_sanitized
         else:
             data.postscript_name = None
 
     ufo_glyphdata_manager.__main__.write_csv(csv_path, glyphdata)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
