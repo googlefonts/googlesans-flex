e4db88f197739bd81b2c0256fa3ef5cfc2d1bf89
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2023-03-10 13:35:17 +0000

    Reduce import glyph list

--- .github/actions/import/entrypoint.py
+++ .github/actions/import/entrypoint.py
@@ -22,7 +22,7 @@ from scripts.gs_merge_designspace import main as merge
 from scripts.gs_normalize_designspace import main as normalize
 from scripts.fix_metadata_in_sources import main as fix_metadata
 
-REPLACE_TARGET_DESIGNSPACE = True
+REPLACE_TARGET_DESIGNSPACE = False
 FOLLOW_GLYPHS = True  # while sources are in flux
 
 WORKSPACE_ROOT = Path("/github/workspace")
