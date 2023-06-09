e6b0e1044e519a30f09f8d8a2d313f8b2fa648cb
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2023-06-09 12:36:17 +0100

    Remove GRAD axis

--- scripts/one-off/print-space-width.py
+++ scripts/one-off/print-space-width.py
@@ -31,19 +31,17 @@ csv_writer.writerow(("", *(f"{wght=}" for wght in WGHTS)))
 for opsz in (6.0, 17.999, 18.0, 144.0):
     for wdth in (25.0, 39.999, 40.0, 50.0, 85.0, 100.0, 151.0):
         for rond in (0.0, 100.0):
-            for grad in (-50.0, 0.0, 50.0):
-                index_str = f"{rond=}, {grad=}, {opsz=}, {wdth=}"
-                wght_wdths = []
+            index_str = f"{rond=}, {opsz=}, {wdth=}"
+            wght_wdths = []
 
-                for wght in WGHTS:
-                    gs = font.getGlyphSet(
-                        location={
-                            "ROND": rond,
-                            "GRAD": grad,
-                            "opsz": opsz,
-                            "wdth": wdth,
-                            "wght": wght,
-                        }
-                    )
-                    wght_wdths.append(otRound(gs["space"].width))
-                csv_writer.writerow((index_str, *wght_wdths))
+            for wght in WGHTS:
+                gs = font.getGlyphSet(
+                    location={
+                        "ROND": rond,
+                        "opsz": opsz,
+                        "wdth": wdth,
+                        "wght": wght,
+                    }
+                )
+                wght_wdths.append(otRound(gs["space"].width))
+            csv_writer.writerow((index_str, *wght_wdths))
