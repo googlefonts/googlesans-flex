0375645af2ee3a87474614ceba73d5f8180848cc
Dave Crossland <dave@lab6.com>
2023-07-14 03:21:23 -0600

    Add new -no-optimize-gvar builds

--- sources/Regular/test-Gk/3 Source/build.sh
+++ sources/Regular/test-Gk/3 Source/build.sh
@@ -1,1 +1,2 @@
-fontmake -m GSF-3style.designspace.xml -o variable;
+fontmake -m GSF-3style.designspace.xml -o variable --output-path variable_ttf/GSF-3style.designspace-VF.ttf;
+fontmake -m GSF-3style.designspace.xml -o variable --output-path variable_ttf/GSF-3style.designspace-VF-no-optimize-gvar.ttf --no-optimize-gvar;
--- sources/Regular/test-Gk/5 Source/build.sh
+++ sources/Regular/test-Gk/5 Source/build.sh
@@ -1,2 +1,5 @@
-fontmake -m GSF-4style.designspace.xml -o variable;
-fontmake -m GSF-5style.designspace.xml -o variable;
+fontmake -m GSF-4style.designspace.xml -o variable --output-path variable_ttf/GSF-4style.designspace-VF.ttf;
+fontmake -m GSF-4style.designspace.xml -o variable --output-path variable_ttf/GSF-4style.designspace-VF-no-optimize-gvar.ttf --no-optimize-gvar;
+
+fontmake -m GSF-5style.designspace.xml -o variable --output-path variable_ttf/GSF-5style.designspace-VF.ttf;
+fontmake -m GSF-5style.designspace.xml -o variable --output-path variable_ttf/GSF-5style.designspace-VF-no-optimize-gvar.ttf --no-optimize-gvar;
