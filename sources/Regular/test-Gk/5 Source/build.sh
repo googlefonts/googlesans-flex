30e8eef1922539430e30189b9946f5e1342196c4
Dave Crossland <dave@lab6.com>
2023-07-14 08:00:00 -0600

    Add demo where ROND001 is moved much closer to 000. Weirdness results

--- sources/Regular/test-Gk/5 Source/build.sh
+++ sources/Regular/test-Gk/5 Source/build.sh
@@ -3,3 +3,5 @@ fontmake -m GSF-4style.designspace.xml -o variable --output-path variable_ttf/GS
 
 fontmake -m GSF-5style.designspace.xml -o variable --output-path variable_ttf/GSF-5style.designspace-VF.ttf;
 fontmake -m GSF-5style.designspace.xml -o variable --output-path variable_ttf/GSF-5style.designspace-VF-no-optimize-gvar.ttf --no-optimize-gvar;
+
+fontmake -m GSF-5style-ROND0000001.designspace.xml -o variable --output-path variable_ttf/GSF-5style-ROND0000001.designspace-VF-no-optimize-gvar.ttf --no-optimize-gvar;
