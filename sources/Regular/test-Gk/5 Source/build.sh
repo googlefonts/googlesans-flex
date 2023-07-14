939e07ea026ef98c5b5715ea9212d23a6ce3fb0d
Dave Crossland <dave@lab6.com>
2023-07-14 15:45:43 -0600

    Add ROND centered design space
        <axis tag="ROND" name="Round" minimum="0" maximum="100" default="0"/>
    
    to
    
        <axis tag="ROND" name="Round" minimum="0" maximum="100" default="0.1"/>

--- sources/Regular/test-Gk/5 Source/build.sh
+++ sources/Regular/test-Gk/5 Source/build.sh
@@ -9,3 +9,6 @@ fontmake -m GSF-5style-ROND0000001.designspace.xml -o variable --output-path var
 fontmake -m GSF-5style-ROND0_00006103515.designspace.xml -o variable --output-path variable_ttf/GSF-5style-ROND0_00006103515.designspace-VF-no-optimize-gvar.ttf --no-optimize-gvar;
 
 fontmake -m GSF-3style-ROND0_00006103515.designspace.xml -o variable --output-path variable_ttf/GSF-3style-ROND0_00006103515.designspace-VF-no-optimize-gvar.ttf --no-optimize-gvar;
+
+fontmake -m GSF-5style-ROND001-centered.designspace.xml -o variable --output-path variable_ttf/GSF-3style-ROND001-centered.designspace-VF-no-optimize-gvar.ttf --no-optimize-gvar;
+
