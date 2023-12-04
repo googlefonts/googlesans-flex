562a9c3d162c7fda7a2bd33e20672f97eff49228
Harry Dalton <harry.dalton@daltonmaag.com>
2023-12-04 10:57:22 +0000

    Update list of incompatible glyphs (only skip /five.denominator)

--- scripts/glyphs_to_ds.py
+++ scripts/glyphs_to_ds.py
@@ -24,43 +24,7 @@ from tempfile import TemporaryDirectory
 from argparse import ArgumentParser
 
 # Incompatible glyphs, that should not be included in the final build.
-INCOMPATIBLE = set(
-    [
-        "acircumflex.alt",
-        "acircumflex",
-        "Acircumflex",
-        "circumflex",
-        "circumflexcomb",
-        "ecircumflex",
-        "Ecircumflex",
-        "four.denominator",
-        "four.numerator",
-        "four.sinf",
-        "four.tf",
-        "foursubscript",
-        "foursuperior",
-        "foursuperscript",
-        "hungarumlaut",
-        "hungarumlautcomb",
-        "icircumflex",
-        "Icircumflex",
-        "ocircumflex",
-        "Ocircumflex",
-        "ohungarumlaut",
-        "Ohungarumlaut",
-        "onequarter",
-        "t",
-        "threequarters",
-        "ucircumflex",
-        "Ucircumflex",
-        "uhungarumlaut",
-        "Uhungarumlaut",
-        "wcircumflex",
-        "Wcircumflex",
-        "ycircumflex",
-        "Ycircumflex",
-    ]
-)
+INCOMPATIBLE = set(["five.denominator"])
 
 if __name__ == "__main__":
     parser = ArgumentParser()
