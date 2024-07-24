73e78c06d4e0fb87e3574221d90a9b0f26447db3
Jany Belluz <jany.belluz@daltonmaag.com>
2024-07-24 11:51:53 +0100

    Add comment about glyph set from Harry

---
+++ scripts/one-off/diff-kerning/glyphs_app_snippet_glyph_names.py
@@ -0,0 +1,87 @@
+# From Marianna's chat message
+chars="""
+Ĩ
+ĩ
+Ƙ
+Ơ
+ơ
+Ư
+ư
+Ƴ
+ƴ
+ɓ
+ɗ
+ʻ
+ʼ
+Ỉ
+ỉ
+Ị
+ị
+Ớ
+ớ
+Ờ
+ờ
+Ở
+ở
+Ỡ
+ỡ
+Ợ
+ợ
+Ứ
+ứ
+Ừ
+ừ
+Ử
+ử
+Ữ
+ữ
+Ự
+ự
+ỵ
+Ĩ
+ĩ
+Ɓ
+Ɗ
+Ə
+ə
+ʻ
+ʼ
+Ỉ
+ỉ
+Ị
+ị
+Ấ
+ấ
+Ầ
+ầ
+Ẩ
+ẩ
+Ẫ
+ẫ
+Ắ
+ắ
+Ằ
+ằ
+Ẳ
+ẳ
+Ẵ
+ẵ
+Ế
+ế
+Ề
+ề
+Ể
+ể
+Ễ
+ễ
+"""
+code_point_to_name = {}
+for g in Glyphs.font.glyphs:
+	for u in g.unicodes or []:
+		code_point_to_name[int(u,16)]=g.name
+print(code_point_to_name)
+for c in chars.splitlines():
+	if not c: continue
+	code_point = ord(c[0])
+	print(code_point_to_name[code_point])
+	
--- scripts/one-off/diff-kerning/split_kerning_groups.py
+++ scripts/one-off/diff-kerning/split_kerning_groups.py
@@ -32,6 +32,11 @@ old_names = {glyph.name for glyph in old_font.glyphs}
 new_names = {glyph.name for glyph in new_font.glyphs}
 to_partition = new_names - old_names
 
+# NOTE: (Jany) after review of the set computed automatically above, it's exhaustive
+# An optimization later could be to use a reduced set. See this sheet:
+# https://docs.google.com/spreadsheets/d/1rWfuGv8KEkYPyA30wD2hJctMtMC5VRE1HSTlSkKN1B0/edit?gid=1037277274#gid=1037277274
+
+
 # See which glyphs are in which group.
 left_groups: dict[str, set[str]] = {}
 right_groups: dict[str, set[str]] = {}
