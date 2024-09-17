ec87c43791633a570850805c1ab6930bb0d9f885
Harry Dalton <harry.dalton@daltonmaag.com>
2024-09-17 14:03:35 +0100

    Add script to find locations that contribute to HVAR for a given glyph
    For chasing #1040

---
+++ scripts/one-off/interesting_hvar_locs.py
@@ -0,0 +1,73 @@
+# Copyright 2024 Google Sans Flex authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Get the location tuples that contribute to a glyph's HVAR."""
+
+from __future__ import annotations
+
+from argparse import ArgumentParser
+
+from fontTools.ttLib import TTFont
+from fontTools.ttLib.tables.H_V_A_R_ import table_H_V_A_R_ as Hvar
+
+AxisToTuples = dict[str, set[tuple[float]]]
+AxisToCoords = dict[str, set[float]]
+
+
+def get_interesting_locs(ttf: TTFont, glyph: str) -> tuple[AxisToCoords, AxisToTuples]:
+    hvar: Hvar = ttf["HVAR"]
+
+    # Follow the mapping to get the glyph's variation.
+    varidx = hvar.table.AdvWidthMap.mapping[glyph]
+    major, _minor = varidx >> 16, varidx & 0xFFFF
+    data = hvar.table.VarStore.VarData[major]
+
+    # Follow the region indices to get the glyph's regions.
+    regions = [
+        hvar.table.VarStore.VarRegionList.Region[idx].VarRegionAxis
+        for idx in data.VarRegionIndex
+    ]
+
+    tags = [axis.axisTag for axis in ttf["fvar"].axes]
+
+    # Fetch the location tuples for each axis.
+    tups = {
+        tag: {(tup.StartCoord, tup.PeakCoord, tup.EndCoord) for tup in tuples}
+        for tag, tuples in zip(tags, zip(*regions))
+    }
+
+    # Summarise the coordinates seen in these tuples.
+    interesting_locs = {
+        tag: {pos for tup in tuples for pos in tup} for tag, tuples in tups.items()
+    }
+
+    return interesting_locs, tups
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("ttf", type=TTFont)
+    parser.add_argument("glyph", type=str)
+    args = parser.parse_args()
+
+    locs, tups = get_interesting_locs(args.ttf, args.glyph)
+
+    print("HVAR tuples:")
+    for tag, tups in tups.items():
+        print(tag, sorted(tups))
+    print()
+
+    print("HVAR locations:")
+    for tag, locs in locs.items():
+        print(tag, sorted(locs))
