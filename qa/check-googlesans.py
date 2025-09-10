8a01f4646c06d23978e1ed048258f0d318e02995
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2025-09-10 17:50:21 +0100

    Drop fontbakery googlefonts profile
    Simon says all relevant checks have been ported to fontspector.

--- qa/check-googlesans.py
+++ qa/check-googlesans.py
@@ -12,41 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
-import subprocess
-import tomllib
 from pathlib import Path
 
-from fontbakery.profiles.googlefonts import PROFILE as GOOGLEFONTS_PROFILE
-
-# Try and exclude all checks that are covered by fontspector.
-try:
-    data_json = subprocess.check_output(
-        ["fontspector", "--profile", "googlefonts", "--list-checks-json"]
-    )
-    data = json.loads(data_json)
-    fontspector_checks = [check["id"] for _, checks in data.items() for check in checks]
-    print("INFO: Skipping checks covered by fontspector.")
-except Exception:
-    fontspector_checks = []
-    print(
-        "INFO: fontspector not found or something else happened; running checks covered by fontspector."
-    )
-
-# NOTE: Consider the fontspector configuration the source of truth. We want to
-# phase out the use of fontbakery.
-CHECKS_TO_IGNORE_TOML = Path(__file__).parent / "check-googlesans.toml"
-CHECKS_TO_IGNORE = tomllib.loads(CHECKS_TO_IGNORE_TOML.read_text())["exclude_checks"]
-
-# Hack to have this be conditional but without appending later
-FONTBAKERY_UP_TO_DATE = (
-    ["fontbakery_version"]
-    if (Path(__file__).parent.parent / "requirements-fb.txt").exists()
-    else []
-)
 
 PROFILE = {
-    "include_profiles": ["googlefonts"],
     "check_definitions": [Path(__file__).parent / "checks" / "googlesans.py"],
     "sections": {
         "Google Sans Flex Custom Checks": [
@@ -59,21 +28,9 @@ PROFILE = {
             "googlesansflex/android_ymin_ymax",
             "googlesansflex/android_hvar",
             "googlesansflex/varfont/has_HVAR",
-        ]
-    },
-    "exclude_checks": [
-        *GOOGLEFONTS_PROFILE["sections"]["Outline Checks"],  # Separate.
-        *FONTBAKERY_UP_TO_DATE,
-        *CHECKS_TO_IGNORE,
-        *fontspector_checks,
-    ],
-    "overrides": {
-        "varfont/consistent_axes": [
-            {
-                "code": "missing-axis",
-                "status": "WARN",
-                "reason": "It's intended as slnt becomes italics",
-            }
-        ]
+        ],
+        "Fontbakery checks": [
+            "ots",
+        ],
     },
 }
