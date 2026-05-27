2f7008b98f069a8a90e63d7bb1e202d31108dff1
RickyDaMa <ricky.atkins@daltonmaag.com>
2026-05-27 09:53:19 +0100

    Set up shebangs to use uv to install deps

--- qa/check-charset.py
+++ qa/check-charset.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env -S uv run --script
 
 # Copyright 2020 Google Sans Authors
 
@@ -13,6 +13,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# /// script
+# dependencies = [
+#     "fontspectorapi",
+#     "fontTools",
+# ]
+# ///
 
 from difflib import unified_diff
 from pathlib import Path
